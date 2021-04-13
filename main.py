76a1497de84f5bed43b5a51026228a07996ace49
RoylatGnail <>
2021-04-13 20:03:24 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, send_from_directory
+import os
+app = Flask('app')
+
+
+
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/favicon.ico')
+def favicon():
+    return app.send_static_file('./favicon.ico')
+
+app.run(host='0.0.0.0', port=8080)
